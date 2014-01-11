c845674c89fa5f0f31dfdefbabf9ca0044ad2ad4
Sam Joseph <tansaku@gmail.com>
2014-01-11 08:31:16 +0000

    other ruby3 mortgage challenges

--- mortgage.rb
+++ mortgage.rb
@@ -1,6 +1,8 @@
 require 'debugger'
 
-def provide_mortgage?(salary, deposit, property_value)
+def provide_mortgage?(salary, deposit, property_value, bankrupt=false)
+  return false if bankrupt
+  return true if deposit/property_value.to_f >= 0.75
   loan_amount = property_value - deposit
   min_deposit = property_value < 650000 ? 0.05 : 0.2 # 20%
   max_multiplier = 5 # how many annual incomes can be borrowed
@@ -17,10 +19,16 @@ raise 'failed to provide mortgage for >5% deposit when value < 650K' unless prov
 raise 'failed to reject mortgage for <5% deposit when value < 650K' if provide_mortgage? 120000, 29000, 600000
 raise 'failed to provide mortgage for >20% deposit when value > 650K' unless provide_mortgage? 120000, 140000, 700000
 raise 'failed to reject mortgage for <20% deposit when value > 650K' if provide_mortgage? 120000, 31000, 700000
-
+# note we can see split into unit and acceptance tests here if we have a calculate_min_deposit method
 
 #Extend the provide_mortgage?() method to waive the limit on the loan amount if the deposit is 
 #75% or more.
 
+raise 'failed to waive limit for >75% deposit when value < 650K' unless provide_mortgage? 1, 450000, 600000
+raise 'failed to maintain limit for <75% deposit when value > 650K' if provide_mortgage? 1, 449999, 700000
+
+
 #Extend the method to accept another argument, a boolean value indicating if the borrower is 
 #currently bankrupt. Modify the method to never provide a mortgage to a bankrupt person.
+
+raise 'failed to reject mortgage when borrower is bankrupt' if provide_mortgage? 120000, 31000, 600000, true
